c1b2c1b83a7aa2c7dfba8c8827fb328a9b54452f
henry <cheffe01@web.de>
2020-08-10 15:21:51 +0200

    add further SIGNALS, add Email2JiraImpl class

---
+++ src/Email2JiraImpl.cpp
@@ -0,0 +1,6 @@
+#include "Email2JiraImpl.h"
+
+Email2JiraImpl::Email2JiraImpl()
+{
+
+}
---
+++ src/Email2JiraImpl.h
@@ -0,0 +1,11 @@
+#ifndef EMAIL2JIRAIMPL_H
+#define EMAIL2JIRAIMPL_H
+
+
+class Email2JiraImpl
+{
+public:
+    Email2JiraImpl();
+};
+
+#endif // EMAIL2JIRAIMPL_H
